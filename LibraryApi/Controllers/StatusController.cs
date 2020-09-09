b8fa46fbc3345f0d7f7bd2e46766ea035627ee65
Jeffry Gonzalez <jeff@hypertheory.com>
2020-09-09 15:02:53 -0400

    Add project files.

---
+++ LibraryApi/Controllers/StatusController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+
+        
+        // GET /status -> StatusController#GetStatus
+        [HttpGet("/status")]
+        public ActionResult GetStatus()
+        {
+            var status = new StatusResponse
+            {
+                Message = "Looks good on my end. Party On!",
+                CheckedBy = "Joe Schmidt",
+                WhenChecked = DateTime.Now
+            };
+
+            return Ok(status);
+        }
+    }
+
+    public class StatusResponse
+    {
+        public string Message { get; set; }
+        public string CheckedBy { get; set; }
+        public DateTime WhenChecked { get; set; }
+    }
+}
